d36d92c85b7f9a6c804254c1d85a84f823e14238
Vladimir Galaiko <a100war@gmail.com>
2024-01-07 16:30:19 +0200

    com 1
    com 1

--- src/2.ts
+++ src/2.ts
@@ -10,30 +10,48 @@
 
   Клас Manager повинен бути підклас класу Employee
 
-  Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
+  Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу 
+  та збільшуватиме salary на 10000.
 
 */
 
-class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+// class Employee {
+//   // Заповніть модифікатори доступу
+//   name: string;
+//   department: string;
+//   salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+//   constructor(name: string, department: string, salary: number) {
+//     this.name = name;
+//     this.department = department;
+//     this.salary = salary;
+//   }
+
+//   getEmployeeDetails() {
+//     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+//   }
+// }
+
+// class Manager extends Employee {
+//   // Реалізуйте конструктор та збільшіть salary на 10000
+// }
+
+class Employee {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
-    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`
   }
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(salary: number) {
+    super('Bill', 'First', salary + 1000)
+  }
 }
 
-
-export {};
+export {}
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,58 @@
-const key = new Key();
+// Ключ (Key): Створіть клас Key. У нього має бути одна приватна властивість signature,
+//  яка генерується випадково при створенні об'єкта цього класу (наприклад Math.random()).
+// Також цей клас повинен мати метод getSignature, який повертає значення властивості signature.
 
-const house = new MyHouse(key);
-const person = new Person(key);
+class Key {
+  private signature: number
+  constructor() {
+    this.signature = Math.random()
+  }
+  getSignature() {
+    return this.signature
+  }
+}
 
-house.openDoor(person.getKey());
+class Person {
+  constructor(private key: Key) {
+    this.key = key
+  }
+  getKey() {
+    return this.key.getSignature()
+  }
+}
 
-house.comeIn(person);
+abstract class House {
+  door: boolean
+  key: Key
+  tenants: Person[] = []
+  constructor(key: Key) {
+    this.door = false
+    this.key = key
+  }
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person)
+    }
+  }
+  public abstract openDoor(keySignature: number): boolean
+}
 
+class MyHouse extends House {
+  openDoor(enteredKeySignature: number): boolean {
+    if (enteredKeySignature === this.key.getSignature()) {
+      this.door = true
+      return true
+    }
+    return false
+  }
+}
+const key = new Key()
 
-export {};
+const house = new MyHouse(key)
+const person = new Person(key)
+
+house.openDoor(person.getKey())
+
+house.comeIn(person)
+
+export {}
